d02a2873d80112300f730135cd0e9bdc0cb01209
Luis Plana <luis.plana@manchester.ac.uk>
2018-05-05 17:50:53 +0100

    - removed debugging code left over.

--- c_code/input.c
+++ c_code/input.c
@@ -262,11 +262,6 @@ void done (uint ec)
       #endif
 
       break;
-
-    case SPINN_PKT_QUEUE_ERROR:
-      io_printf (IO_BUF, "unexpected packet in queue - abort!\n");
-
-      break;
   }
 
   // report diagnostics
