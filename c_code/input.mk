ad8d31612e5aa0fcdb115dc4fbb5b6520b25a2ff
Luis A. Plana <luis.plana@manchester.ac.uk>
2020-05-01 19:22:49 +0100

    first steps to upgrade. C code now compiles correctly.

---
+++ c_code/input.mk
@@ -0,0 +1,21 @@
+# input core makefile
+
+# The name of the application to be built
+APP = input
+
+# Directory to create APLX files in (must include trailing slash)
+APP_OUTPUT_DIR = ../binaries/
+
+SOURCE_DIRS = .
+SOURCES = input.c comms_i.c process_i.c init_i.c activation.c
+
+LIBRARIES += -lm
+
+# The GFE application standard makefile
+include $(SPINN_DIRS)/make/local.mk
+
+all: $(APP_OUTPUT_DIR)$(APP).aplx
+
+# Tidy up
+tidy:
+	$(RM) $(OBJECTS) $(BUILD_DIR)$(APP).elf $(BUILD_DIR)$(APP).txt
---
+++ c_code/sum.mk
@@ -0,0 +1,21 @@
+# sum core makefile
+
+# The name of the application to be built
+APP = sum
+
+# Directory to create APLX files in (must include trailing slash)
+APP_OUTPUT_DIR = ../binaries/
+
+SOURCE_DIRS = .
+SOURCES = sum.c comms_s.c process_s.c init_s.c
+
+LIBRARIES += -lm
+
+# The GFE application standard makefile
+include $(SPINN_DIRS)/make/local.mk
+
+all: $(APP_OUTPUT_DIR)$(APP).aplx
+
+# Tidy up
+tidy:
+	$(RM) $(OBJECTS) $(BUILD_DIR)$(APP).elf $(BUILD_DIR)$(APP).txt
