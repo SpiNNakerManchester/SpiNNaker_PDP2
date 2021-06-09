933ea861f15dfbe51b5a7b041c8f47bc43386713
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-06-09 08:17:37 +0100

    remove unwanted params

--- spinn_pdp2/weight_vertex.py
+++ spinn_pdp2/weight_vertex.py
@@ -277,7 +277,7 @@ class WeightVertex(
     @overrides(MachineDataSpecableVertex.generate_machine_data_specification)
     def generate_machine_data_specification(
             self, spec, placement, machine_graph, routing_info, iptags,
-            reverse_iptags, machine_time_step, time_scale_factor):
+            reverse_iptags):
 
         # Generate the system data region for simulation.c requirements
         generate_steps_system_data_region(spec, MLPRegions.SYSTEM.value, self)
