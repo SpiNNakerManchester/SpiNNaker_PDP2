08c2d103417e77b4ac66cfcd2d38377fa4fa147e
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-01-10 16:58:26 +0000

    View.get_routing_infos

--- spinn_pdp2/threshold_vertex.py
+++ spinn_pdp2/threshold_vertex.py
@@ -380,9 +380,9 @@ class ThresholdVertex(
 
     @overrides(MachineDataSpecableVertex.generate_machine_data_specification)
     def generate_machine_data_specification(
-            self, spec, placement, machine_graph, routing_info, iptags,
-            reverse_iptags):
+            self, spec, placement, iptags, reverse_iptags):
 
+        routing_info = FecDataView.get_routing_infos()
         # Generate the system data region for simulation.c requirements
         generate_steps_system_data_region(spec, MLPRegions.SYSTEM.value, self)
 
