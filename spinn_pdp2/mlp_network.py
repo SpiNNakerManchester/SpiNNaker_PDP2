b84a084b4cc040ad61eb1ce604cefb96d9752aa6
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2022-03-17 08:16:07 +0000

    fixed mistakes changing to FecDataView

--- spinn_pdp2/mlp_network.py
+++ spinn_pdp2/mlp_network.py
@@ -666,7 +666,7 @@ class MLPNetwork():
                         gtv = g.t_vertex[s]
                         try:
                             rec_outputs[g.write_blk].append (gtv.read (
-                                FecDataView.get_placement_of_vertex(ftv),
+                                FecDataView.get_placement_of_vertex(gtv),
                                 gfe.buffer_manager(),
                                 MLPVarSizeRecordings.OUTPUTS.value)
                                 )
@@ -801,7 +801,7 @@ class MLPNetwork():
             ltv = g.t_vertex[g.subgroups - 1]
             try:
                 rec_test_results = ltv.read (
-                    FecDataView.get_placement_of_vertex(ftv),
+                    FecDataView.get_placement_of_vertex(ltv),
                     gfe.buffer_manager(), MLPConstSizeRecordings.TEST_RESULTS.value
                     )
             except Exception as err:
