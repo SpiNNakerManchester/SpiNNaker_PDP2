457cab3875731b6be799aa2f313df2f958418aca
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-12-17 10:24:23 +0000

    guess FEC_INSTALL_DIR

--- c_code/input.mk
+++ c_code/input.mk
@@ -15,7 +15,11 @@
 
 # input core makefile
 
-FEC_INSTALL_DIR := $(strip $(if $(FEC_INSTALL_DIR), $(FEC_INSTALL_DIR), $(if $(SPINN_DIRS), $(SPINN_DIRS)/fec_install, $(error FEC_INSTALL_DIR or SPINN_DIRS is not set.  Please define FEC_INSTALL_DIR or SPINN_DIRS))))
+ifndef FEC_INSTALL_DIR:
+    CUR_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/)
+    # assume parallel clone
+    FEC_INSTALL_DIR := $(abspath $(CUR_DIR)/../../SpiNNFrontEndCommon/c_common/front_end_common_lib)
+endif
 
 # The name of the application to be built
 APP = input
